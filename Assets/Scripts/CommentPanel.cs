98cb879a7079bd076db2f190491ff8d62897ae11
TsutayaJun <dominopizza.0913@icloud.com>
2021-08-03 22:56:11 +0900

    追加：吹き出し機能を追加

---
+++ Assets/Scripts/CommentPanel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CommentPanel : MonoBehaviour
+{
+    [SerializeField] string[] m_coments;
+    int m_comentIndex;
+    [SerializeField] Text m_text;
+
+    private void OnEnable()
+    {
+        m_comentIndex = 0;
+    }
+
+    public void OnClick()
+    {
+        if (m_coments.Length > 0 &&
+            m_comentIndex < m_coments.Length)
+        {
+            m_text.text = m_coments[m_comentIndex];
+            m_comentIndex++;
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
